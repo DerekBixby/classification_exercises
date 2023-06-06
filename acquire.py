8caae1fcd27c3e941636ec25dda9ba3a954addcb
Derek Bixby <derek.s.bixby@gmail.com>
2023-06-06 11:59:14 -0500

    added acquire.py with final functions

--- acquire.py
+++ acquire.py
@@ -8,13 +8,13 @@ def new_titanic_data():
 def get_titanic_data():
     filename = "titanic.csv"
 
-    if os.path.isfile(filename):
-        return pd.read_csv(filename)
+    if os.path.isfile('titanic_dataframe.csv'):
+        return pd.read_csv('titanic_dataframe.csv', index_col=0)
     
     else:
         url = env.get_db_url('titanic_db')
         df_titanic = pd.read_sql('SELECT * FROM passengers', url)
-        df_titanic.to_csv(filename)
+        df_titanic.to_csv('titanic_dataframe.csv')
         return df_titanic
 
 def new_iris_data(): 
@@ -28,8 +28,8 @@ def new_iris_data():
 def get_iris_data(): 
     filename = "iris.csv"
 
-    if os.path.isfile(filemname):
-        return pd.read_csv(filename)
+    if os.path.isfile('iris_data.csv'):
+        return pd.read_csv('iris_data.csv', index_col=0)
     
     else: 
         url = env.get_db_url('iris_db')
@@ -37,7 +37,7 @@ def get_iris_data():
         SELECT * FROM measurements
         JOIN species ON measurements.species_id = species.species_id
         ''', url)
-        df_iris.to_csv(filename)
+        df_iris.to_csv('iris_data.csv')
         return df_iris
     
 def new_telco_data(): 
@@ -53,8 +53,8 @@ def new_telco_data():
 def get_telco_data(): 
     filename = "telco_churn.csv"
 
-    if os.path.isfile(filemname):
-        return pd.read_csv(filename)
+    if os.path.isfile('telco_data.csv'):
+        return pd.read_csv('telco_data.csv', index_col=0)
     
     else: 
         url = env.get_db_url('telco_churn')
@@ -64,5 +64,5 @@ def get_telco_data():
         JOIN customer_payments ON customers.customer_id = customer_payments.customer_id
         JOIN customer_contracts ON customers.customer_id = customer_contracts.customer_id
         ''', url)
-        df_telco.to_csv(filename)
+        df_telco.to_csv('telco_data.csv')
         return df_telco
